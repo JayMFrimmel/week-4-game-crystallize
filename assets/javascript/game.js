1d4684bb4aa76651098ec26d2ed0b0e13d44a923
JayMFrimmel <jayfrimmel@yahoo.com>
2016-08-13 16:51:46 -0400

    fourth commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var randomNum = Math.ceil((Math.random() * 10) + 1); console.log, 
+var el = document.getElementById('Ruby'); 
+el.innerHTML = '<h3>Ruby/><p>' + randomNum + '</p>';
+
+var randomNum = Math.ceil((Math.random() * 10) + 1);
+var el = document.getElementById('Diamond');console.log,
+el.innerHTML = '<h3>Diamond</><p>' + randomNum + '</p>';
+
+var randomNum = Math.ceil((Math.random() * 10) + 1);
+Var el = document.getElementById('Sapphire');console.log,
+el.innerHTML = '<h3>Sapphire</><p>' + randomNum + '</p>';
+
+var randomNum = Math.ceil((Math.random() * 10) + 1);
+var el = document.getElementById('Emerald'); consloe.log,
+el.innerHTML = '<h3>Emerald</><p>' + randomNum + '</p>';
